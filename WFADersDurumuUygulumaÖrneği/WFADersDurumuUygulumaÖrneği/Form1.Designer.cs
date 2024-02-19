43e458ace58c907084289895e3e1f2218f4c6ef6
ibrahimdeniz24 <160463714+ibrahimdeniz24@users.noreply.github.com>
2024-02-19 16:56:35 +0300

    dersDurumuUyg
    Dersin geçip geçmediğimi sağlayan uygulama

---
+++ WFADersDurumuUygulumaÖrneği/WFADersDurumuUygulumaÖrneği/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace WFADersDurumuUygulumaÖrneği
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblDurum = new System.Windows.Forms.Label();
+            this.lblOrtalama = new System.Windows.Forms.Label();
+            this.txtSinav1 = new System.Windows.Forms.TextBox();
+            this.txtSinav2 = new System.Windows.Forms.TextBox();
+            this.txtSinav3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1.Sınav :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "2.Sınav :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 19);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "3.Sınav :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(329, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 19);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Ortalama :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(338, 51);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 19);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Durumu :";
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.AutoSize = true;
+            this.lblDurum.Location = new System.Drawing.Point(411, 51);
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(43, 19);
+            this.lblDurum.TabIndex = 0;
+            this.lblDurum.Text = "NULL";
+            // 
+            // lblOrtalama
+            // 
+            this.lblOrtalama.AutoSize = true;
+            this.lblOrtalama.Location = new System.Drawing.Point(411, 13);
+            this.lblOrtalama.Name = "lblOrtalama";
+            this.lblOrtalama.Size = new System.Drawing.Size(25, 19);
+            this.lblOrtalama.TabIndex = 0;
+            this.lblOrtalama.Text = "00";
+            // 
+            // txtSinav1
+            // 
+            this.txtSinav1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSinav1.Location = new System.Drawing.Point(75, 10);
+            this.txtSinav1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtSinav1.Name = "txtSinav1";
+            this.txtSinav1.Size = new System.Drawing.Size(167, 27);
+            this.txtSinav1.TabIndex = 2;
+            // 
+            // txtSinav2
+            // 
+            this.txtSinav2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSinav2.Location = new System.Drawing.Point(75, 47);
+            this.txtSinav2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtSinav2.Name = "txtSinav2";
+            this.txtSinav2.Size = new System.Drawing.Size(167, 27);
+            this.txtSinav2.TabIndex = 2;
+            // 
+            // txtSinav3
+            // 
+            this.txtSinav3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSinav3.Location = new System.Drawing.Point(75, 89);
+            this.txtSinav3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtSinav3.Name = "txtSinav3";
+            this.txtSinav3.Size = new System.Drawing.Size(167, 27);
+            this.txtSinav3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 133);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(458, 27);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "HESAPLA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 202);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtSinav3);
+            this.Controls.Add(this.txtSinav2);
+            this.Controls.Add(this.txtSinav1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblDurum);
+            this.Controls.Add(this.lblOrtalama);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Font = new System.Drawing.Font("Bahnschrift SemiBold SemiConden", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblDurum;
+        private System.Windows.Forms.Label lblOrtalama;
+        private System.Windows.Forms.TextBox txtSinav1;
+        private System.Windows.Forms.TextBox txtSinav2;
+        private System.Windows.Forms.TextBox txtSinav3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WFADersDurumuUygulumaÖrneği/WFADersDurumuUygulumaÖrneği/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFADersDurumuUygulumaÖrneği
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            { 
+                    int sinav1 = Convert.ToInt32(txtSinav1.Text);
+
+                    int sinav2 = Convert.ToInt32(txtSinav2.Text);
+
+                    int sinav3 = Convert.ToInt32(txtSinav3.Text);
+
+                    int ortalama = (sinav1 + sinav2 + sinav3) / 3;
+
+                    lblOrtalama.Text = ortalama.ToString();
+
+
+
+                    if ( ortalama >= 50 )
+                    {
+                         lblDurum.Text = "Geçti";
+                    }
+                    else
+                    {
+                         lblDurum.Text = "Kaldı";
+                    }
+
+            }
+
+            catch  
+            {
+                MessageBox.Show("LÜTFEN SAYISAL DEĞER GİRİNİZ !!!");
+                    
+            }
+        }     
+    }
+}
